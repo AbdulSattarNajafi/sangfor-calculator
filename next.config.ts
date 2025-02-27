134ccef840002f98379f9f22072f44edb8a16b49
AbdulSattarNajafi <66928318+AbdulSattarNajafi@users.noreply.github.com>
2025-02-28 02:13:45 +0700

    assets prefix added

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // basePath: "/roi-calculator",
-  // assetPrefix: "/roi-calculator/",
+  basePath: "/calculator",
+  assetPrefix: "/calculator",
   // trailingSlash: true,
 };
 
