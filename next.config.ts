04006670b06c7f15094a879714daaa1eb39c7c2a
AbdulSattarNajafi <66928318+AbdulSattarNajafi@users.noreply.github.com>
2025-02-28 02:31:57 +0700

    prefix fixed

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/calculator",
-  assetPrefix: "/calculator",
+  assetPrefix: "/calculator/",
   // trailingSlash: true,
 };
 
