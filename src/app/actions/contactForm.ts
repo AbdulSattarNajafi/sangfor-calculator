c1261bfb6faa89fa5714b934c67d2af8aa912d4e
AbdulSattarNajafi <66928318+AbdulSattarNajafi@users.noreply.github.com>
2025-03-12 16:12:19 +0700

    external redirect removed from server

--- src/app/actions/contactForm.ts
+++ src/app/actions/contactForm.ts
@@ -349,8 +349,5 @@ export async function createEloquaEmail(
     throw new Error(`Failed to submit form: ${response.statusText}`);
   }
 
-  // redirect(`/redirect/${userId}`);
-  redirect(
-    `https://www.sangfor.com/cybersecurity/products/sangfor-access-sase/sase-roi-calculator-submission-success-download-report?result=${userId}`,
-  );
+  redirect(`/redirect/${userId}`);
 }
